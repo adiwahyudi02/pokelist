d8f4ad653471ed2434657591fbf71e476176a1c8
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-25 22:29:35 +0700

    feat: add generate image url for pokemon based on id pokemon

---
+++ src/utils/generatePokemonImageUrl.ts
@@ -0,0 +1,5 @@
+const BASE_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork'
+
+export default function generateImageUrl(id: number) {
+    return `${BASE_URL}/${id}.png`
+}
