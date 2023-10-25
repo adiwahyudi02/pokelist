b9eda7e0c06139a8bb526f70fc5a2e11f24d4b28
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-26 00:40:55 +0700

    feat: add Progress indicator component

---
+++ src/components/commons/Progress.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps<{
+    precent: string
+}>()
+</script>
+
+<template>
+    <div class="w-full h-3 bg-slate-100 rounded-lg">
+        <div class="h-3 bg-purple-900 rounded" :style="`width: ${precent}`"></div>
+    </div>
+</template>
