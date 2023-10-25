d70832610a0aa818930936817da7b7aaee3c9299
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-25 23:43:46 +0700

    feat: get pokemon detail data based on param pokemon id

--- src/views/PokemonDetailsView.vue
+++ src/views/PokemonDetailsView.vue
@@ -1,3 +1,31 @@
+<script setup lang="ts">
+import { getPokemon } from '@/services/pokemonServices'
+import type { GetPokemonResType } from '@/types/pokemonType'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const isLoading = ref(false)
+const pokemon = ref<GetPokemonResType>(null!)
+
+const { params } = useRoute();
+
+const fetchPokemon = async () => {
+    try {
+        isLoading.value = true
+        const data = await getPokemon(Number(params.id))
+        pokemon.value = data
+    } catch (e) {
+        console.log(e)
+    } finally {
+        isLoading.value = false
+    }
+}
+
+onMounted(() => {
+    fetchPokemon()
+})
+
+</script>
 <template>
     <h1>Pokemon Detail</h1>
 </template>
