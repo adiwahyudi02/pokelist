72752054f8edcbb7a8c3cdfc9040b9416bd4af0f
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-26 00:17:40 +0700

    feat: add Pokemon Summary Detail

---
+++ src/components/pokemon/PokemonSummary.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+defineProps<{
+    types?: string
+    height?: number
+    weight?: number
+}>()
+</script>
+
+<template>
+    <div class="w-full flex p-4 bg-purple-100 rounded-lg">
+        <div v-if="types" class="flex-1 flex flex-col items-center justify-between gap-2">
+            <p class="font-bold text-sm">{{ types }}</p>
+            <p class="text-xs text-gray-600">Type</p>
+        </div>
+        <div v-if="height" class="flex-1 flex flex-col items-center justify-between gap-2">
+            <p class="text-lg font-bold">{{ height }} m</p>
+            <p class="text-xs text-gray-600">Height</p>
+        </div>
+        <div v-if="weight" class="flex-1 flex flex-col items-center justify-between gap-2">
+            <p class="text-lg font-bold">{{ weight }} kg</p>
+            <p class="text-xs text-gray-600">Weight</p>
+        </div>
+    </div>
+</template>
--- src/views/PokemonDetailsView.vue
+++ src/views/PokemonDetailsView.vue
@@ -1,8 +1,10 @@
 <script setup lang="ts">
+import PokemonSummary from '@/components/pokemon/PokemonSummary.vue'
 import { getPokemon } from '@/services/pokemonServices'
 import type { GetPokemonResType } from '@/types/pokemonType'
-import { onMounted, ref } from 'vue'
+import { computed, onMounted, ref } from 'vue'
 import { useRoute } from 'vue-router'
+import VueLoadImage from 'vue-load-image'
 
 const isLoading = ref(false)
 const pokemon = ref<GetPokemonResType>(null!)
@@ -21,11 +23,39 @@ const fetchPokemon = async () => {
     }
 }
 
+const pokemonTypes = computed(() => {
+    return pokemon.value?.types.map(({ type }) => type.name).join('/')
+})
+
 onMounted(() => {
     fetchPokemon()
 })
 
 </script>
 <template>
-    <h1>Pokemon Detail</h1>
+    <section>
+        <Loading v-if="isLoading" class="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+        <div v-else class="block md:grid md:grid-cols-2 md:gap-5">
+            <div class="md:flex md:flex-col md:justify-between">
+                <div class="w-full flex flex-col items-center justify-center">
+                    <div class="max-w-[14rem]">
+                        <VueLoadImage>
+                            <template v-slot:image>
+                                <img :src="pokemon?.image" alt="Image Pokemon">
+                            </template>
+                            <template v-slot:preloader>
+                                <img src="/pokemon-fallback.png" width="300" height="300" />
+                            </template>
+                        </VueLoadImage>
+                    </div>
+                    <p class="text-2xl font-bold text-purple-800 mt-8">{{ pokemon?.name }}</p>
+                </div>
+                <PokemonSummary :types="pokemonTypes" :height="pokemon?.height" :weight="pokemon?.weight" class="mt-8" />
+            </div>
+            <div class="bg-gray-200">
+                <div>PokemonBaseStats</div>
+                <div>PokemonAbilities</div>
+            </div>
+        </div>
+    </section>
 </template>
