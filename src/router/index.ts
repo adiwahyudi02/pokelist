21a7e9f6e15b9dd42166c67bb43c44e22717027d
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-25 21:47:48 +0700

    feat: add routing - / for pokemon list - /detail/:id for pokemon detail

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import PokemonView from '@/views/PokemonView.vue'
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
-    routes: []
+    routes: [
+        {
+            path: '/',
+            name: 'pokemon-list',
+            component: PokemonView
+        },
+        {
+            path: '/detail/:id',
+            name: 'pokemon-details',
+            component: () => import('@/views/PokemonDetailsView.vue')
+        }
+    ]
 })
 
 export default router
