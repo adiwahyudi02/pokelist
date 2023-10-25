1c59953c6108c581979a462818efb5809500f0db
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-25 21:23:07 +0700

    feat: add layout for all route page

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
+import { RouterView } from 'vue-router'
+import Header from '@/components/commons/Header.vue'
+
 </script>
 
 <template>
-    <h1>Chore: Initial setup of vue project</h1>
+    <div class="max-w-3xl mx-auto p-4">
+        <Header />
+        <main>
+            <RouterView />
+        </main>
+    </div>
 </template>
