f1462128e668f4537596fc9b2238446f0dfed516
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-25 21:21:45 +0700

    feat: add header component

---
+++ src/components/commons/Header.vue
@@ -0,0 +1,8 @@
+<template>
+    <header>
+        <div class="flex flex-col items-center md:items-start">
+            <h1 class="text-3xl font-bold text-purple-800">Pokélist</h1>
+            <h2 class="text-sm text-gray-600">Find your pokemon!</h2>
+        </div>
+    </header>
+</template>
