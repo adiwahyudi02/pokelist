a5262326c6cccd5b4f2687ae97a7011f13227005
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-26 11:27:34 +0700

    feat: add query params for search pokemon list

--- src/views/PokemonView.vue
+++ src/views/PokemonView.vue
@@ -6,6 +6,7 @@ import Loading from '@/components//commons/Loading.vue'
 import { getPokemons } from '@/services/pokemonServices'
 import type { PokemonType } from '@/types/pokemonType'
 import { useInfiniteScroll } from '@/composables/useInfiniteScroll'
+import { useRoute, useRouter } from 'vue-router'
 
 const isLoading = ref(false)
 const pokemons = ref<PokemonType[]>([])
@@ -14,8 +15,12 @@ const scrollComponent = ref()
 const limitPayloadReq = 12
 const offsetPayloadReq = ref(0)
 const searchPayloadReq = ref('')
+const defaultSearch = ref('')
 
 const isDataOut = ref(false)
+
+const router = useRouter()
+const { query } = useRoute()
 
 const fetchPokemons = async () => {
     try {
@@ -45,14 +50,31 @@ const handleLoadMorePokemons = () => {
 
 useInfiniteScroll(scrollComponent, handleLoadMorePokemons)
 
+const handleSetQueryParamsSearch = (search: string) => {
+    if (search) router.push({ path: '/', query: { search } })
+    else router.replace({ path: '/' })
+}
+
+const handleGetQueryParamsSearch = () => {
+    const searchQuery = String(query.search || '')
+
+    if (searchQuery) {
+        defaultSearch.value = searchQuery
+        searchPayloadReq.value = searchQuery
+    }
+}
+
 const handleSearch = (value: string) => {
     searchPayloadReq.value = value
     offsetPayloadReq.value = 0
     pokemons.value = []
+
+    handleSetQueryParamsSearch(value)
     fetchPokemons()
 }
 
 onMounted(() => {
+    handleGetQueryParamsSearch()
     fetchPokemons()
 })
 
@@ -61,7 +83,7 @@ onMounted(() => {
 <template>
     <section>
         <div class="my-5">
-            <SearchInput placeholder="Search by name" @on-search="handleSearch" />
+            <SearchInput placeholder="Search by name" :default-value="defaultSearch" @on-search="handleSearch" />
         </div>
         <div ref="scrollComponent" class="pb-28">
             <div class="grid grid-cols-2 md:grid-cols-4 gap-3">
