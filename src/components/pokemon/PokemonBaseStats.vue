769b01e60e1521be5d0652f6a4b40a4e11ff7dc6
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-26 09:16:30 +0700

    fix: calculation the percentage of pokemon base stats

--- src/components/pokemon/PokemonBaseStats.vue
+++ src/components/pokemon/PokemonBaseStats.vue
@@ -12,9 +12,9 @@ defineProps<{
         <p class="font-bold mb-7">Base Stats</p>
         <div v-for="stat in stats" :key="stat.stat.name" class="flex items-center justify-between gap-2 my-2">
             <p class="w-1/3 md:w-1/2 font-semibold text-sm">{{ stat.stat.name }}</p>
-            <div class="w-full flex items-center gap-2">
-                <p class="text-sm">{{ stat.base_stat }}</p>
-                <Progress :precent="`${(stat.base_stat / 100) * 100}%`" />
+            <div class="w-full flex items-center gap-1">
+                <p class="text-sm w-6 text-end">{{ stat.base_stat }}</p>
+                <Progress :precent="`${(stat.base_stat / 150) * 100}%`" />
             </div>
         </div>
     </div>
