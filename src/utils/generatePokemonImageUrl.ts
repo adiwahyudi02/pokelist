a48f1d6e6240cbc74447ed781c042d25c93ed238
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-26 23:40:12 +0700

    feat: export base url for load pokemon image

--- src/utils/generatePokemonImageUrl.ts
+++ src/utils/generatePokemonImageUrl.ts
@@ -1,4 +1,4 @@
-const BASE_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork'
+export const BASE_URL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork'
 
 export default function generateImageUrl(id: number) {
     return `${BASE_URL}/${id}.png`
