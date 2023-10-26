3eb2fb23ebf2bd6f2a4f2a2df5d74d7c56e803e0
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-26 09:15:16 +0700

    fix: set overflow to hidden when when the percent exceeds capacity

--- src/components/commons/Progress.vue
+++ src/components/commons/Progress.vue
@@ -5,7 +5,7 @@ defineProps<{
 </script>
 
 <template>
-    <div class="w-full h-3 bg-slate-100 rounded-lg">
+    <div class="w-full h-3 bg-slate-100 rounded-lg overflow-x-hidden">
         <div class="h-3 bg-purple-900 rounded" :style="`width: ${precent}`"></div>
     </div>
 </template>
