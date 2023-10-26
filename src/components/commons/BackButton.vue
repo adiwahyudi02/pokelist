2e35b62a5953c610d5cf128ad2b105c707a3b986
adiwahyudi02 <adi.whyd02@gmail.com>
2023-10-26 11:29:12 +0700

    fix: add onBack emit for handle back button

--- src/components/commons/BackButton.vue
+++ src/components/commons/BackButton.vue
@@ -1,21 +1,17 @@
 <script setup lang="ts">
-import { RouterLink, type RouteLocationRaw } from 'vue-router';
-
-defineProps<{
-    to: RouteLocationRaw
+defineEmits<{
+    (event: 'onBack'): void
 }>()
 </script>
 <template>
-    <div>
-        <RouterLink :to="to" class="flex items-center gap-2">
-            <svg class="w-4 h-4 text-purple-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                viewBox="0 0 14 10">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M13 5H1m0 0 4 4M1 5l4-4" />
-            </svg>
-            <p class="font-bold text-purple-800">
-                <slot>Back</slot>
-            </p>
-        </RouterLink>
-    </div>
+    <button type="button" class="flex items-center gap-2" @click="$emit('onBack')">
+        <svg class="w-4 h-4 text-purple-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
+            viewBox="0 0 14 10">
+            <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                d="M13 5H1m0 0 4 4M1 5l4-4" />
+        </svg>
+        <p class="font-bold text-purple-800">
+            <slot>Back</slot>
+        </p>
+    </button>
 </template>
